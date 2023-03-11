fcd2019a37cb5879e10cc0a7572d074ed2ae2209
Subramanian Kumarappan <subramanian.nk@live.com>
2023-03-11 12:01:45 +0530

    open telemetry

---
+++ OpenTelemetryCSharp/OpenTelemetryCSharp/ContactsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OpenTelemetryCSharp
+{
+    public class ContactsAPIDbContext: DbContext
+    {
+
+        public ContactsAPIDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ OpenTelemetryCSharp/OpenTelemetryCSharp/Controllers/ContactsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenTelemetryCSharp.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactsController : ControllerBase
+    {
+    }
+}
